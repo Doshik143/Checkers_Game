159bafd5543f56ad7da5821434caa2662e2be292
Melanchenko_Andriy <ipz232_mad@student.ztu.edu.ua>
2025-06-06 18:55:13 +0300

    refactoring

---
+++ Checkers/Commands/GameCommandInvoker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers.Commands
+{
+    public class GameCommandInvoker
+    {
+        private readonly Stack<IGameCommand> _commandHistory = new Stack<IGameCommand>();
+
+        public void ExecuteCommand(IGameCommand command)
+        {
+            command.Execute();
+            _commandHistory.Push(command);
+        }
+
+        public bool CanUndo => _commandHistory.Count > 0;
+    }
+}
---
+++ Checkers/Strategies/RegularGameStrategy.cs
@@ -0,0 +1,34 @@
+﻿using Checkers.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers.Strategies
+{
+    public class RegularGameStrategy : IGameSessionStrategy
+    {
+        private readonly GameStatistics _stats;
+        private readonly Stopwatch _gameTimer;
+
+        public RegularGameStrategy(GameStatistics stats, Stopwatch gameTimer)
+        {
+            _stats = stats;
+            _gameTimer = gameTimer;
+        }
+
+        public void OnGameOver(PlayerType winner, string style)
+        {
+            _gameTimer.Stop();
+            _stats.RecordGame(winner, _gameTimer.Elapsed);
+            _stats.SaveToFile("stats.dat");
+        }
+
+        public void OnGameStart()
+        {
+            _gameTimer.Restart();
+        }
+    }
+}
